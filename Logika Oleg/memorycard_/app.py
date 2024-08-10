1dbf5909b9a7cf805fe9f59e4091e66ce850de7b
KreisUA <oleggirnij2020@gmail.com>
2024-08-10 17:32:22 +0300

    Add files via upload

--- Logika Oleg/memorycard_/app.py
+++ Logika Oleg/memorycard_/app.py
@@ -1,5 +1,3 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
-
+from PyQt5.QtWidgets import QApplication
 App= QApplication([])
 
--- Logika Oleg/memorycard_/card.py
+++ Logika Oleg/memorycard_/card.py
@@ -1,13 +1,12 @@
-from app import *
+from app import App
+from PyQt5.QtWidgets import QRadioButton,QLabel,QSpinBox,QPushButton,QGroupBox,QHBoxLayout,QVBoxLayout,QButtonGroup
+from PyQt5.QtCore import Qt
 
-win_card = QWidget()
-win_card.resize(600,500)
-win_card.setWindowTitle("Memory Card")
-win_card.move(0,0)
+
 
 btn_sleep = QPushButton("Відпочити")
 box_min = QSpinBox()
-box_min.setValues(5)
+box_min.setValue(5)
 
 lb_ans = QLabel("Запитаня")
 ld_min = QLabel("Мню")
@@ -28,7 +27,7 @@ RadioGroup.addButton(btn_ans2)
 RadioGroup.addButton(btn_ans3)
 RadioGroup.addButton(btn_ans4)
 
-line1 = QVBoxLayout()
+line1 = QHBoxLayout()
 line2 = QVBoxLayout()
 line1.addWidget(btn_menu)
 line1.addWidget(btn_sleep)
@@ -42,16 +41,37 @@ line_btn_ans1.addWidget(btn_ans2)
 line_btn_ans2.addWidget(btn_ans3)
 line_btn_ans2.addWidget(btn_ans4)
 mainline_btn_ans = QHBoxLayout()
-mainline_btn_ans.addWidget(line_btn_ans1)
-mainline_btn_ans.addWidget(line_btn_ans2)
+mainline_btn_ans.addLayout(line_btn_ans1)
+mainline_btn_ans.addLayout(line_btn_ans2)
 AnswersGroupBox.setLayout(mainline_btn_ans)
-
 mani_line =QVBoxLayout()
-mani_line.addWidget(line1)
+mani_line.addLayout(line1)
 mani_line.addWidget(lb_ans)
+
+ResGroupBox = QGroupBox("Результат")
+lb_res = QLabel("Правильність")
+lb_correct = QLabel("Правильна відповідь")
+line_res = QVBoxLayout()
+line_res.addWidget(lb_res)
+line_res.addWidget(lb_correct)
+ResGroupBox.setLayout(line_res)
+mani_line.addWidget(AnswersGroupBox,stretch=8)
+
+mani_line.addWidget(ResGroupBox,stretch=8)
+ResGroupBox.hide()
 mani_line.addWidget(btn_ans)
 
-win_card.setLayout(mani_line)
-
-win_card.show()
-App.exec_()
+def show_res():
+    if btn_ans.text() == "Відоповісти":
+        AnswersGroupBox.hide()
+        ResGroupBox.show()
+        btn_ans.setText("Наступине питаня")
+def show_ans():
+    AnswersGroupBox.show()
+    ResGroupBox.hide()
+    btn_ans.setText("Відповісти")
+    btn_ans1.setChecked(False)
+    btn_ans2.setChecked(False)
+    btn_ans3.setChecked(False)
+    btn_ans4.setChecked(False)
+btn_ans.clicked.connect(show_res)
